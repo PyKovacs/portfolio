9a37af1839c58b14707a72a253e5bc83b84741b6
David Kovacs <davi.kovacs.1991@gmail.com>
2023-04-02 18:15:49 +0200

    added flask app factory, adjusted about me content

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -11,24 +11,28 @@ with open("./portfolio/projects.json", "r") as projects_file:
 
 slug_to_project = {project["slug"]: project for project in projects}
 
-@app.route("/")
-def home():
-    return render_template("home.html", projects=projects)
+def create_app():
+    @app.route("/")
+    def home():
+        return render_template("home.html", projects=projects)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
 
-@app.route("/project/<string:slug>")
-def project(slug):
-    if slug not in slug_to_project:
-        abort(404)
-    return render_template(f"project_{slug}.html", project=slug_to_project[slug])
+    @app.route("/project/<string:slug>")
+    def project(slug):
+        if slug not in slug_to_project:
+            abort(404)
+        return render_template(f"project_{slug}.html", project=slug_to_project[slug])
 
-@app.errorhandler(404)
-def page404(error):
-    return render_template("404.html"), 404
+    @app.errorhandler(404)
+    def page404(error):
+        return render_template("404.html"), 404
+
+
+create_app()
